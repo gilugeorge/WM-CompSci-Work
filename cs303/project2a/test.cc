3f348e2b67a5c220ea28f692b5f71b7585c22117
eshih <ewshih@email.wm.edu>
2012-10-30 22:04:12 -0400

    project2b done

--- cs303/project2a/test.cc
+++ cs303/project2a/test.cc
@@ -13,4 +13,27 @@ int main (){
 	cout<< "=====After Insertion Sort=====" << endl;
 	nums.displayGroupOfNumbers();
 	
+	//Mergesort Test
+	GroupOfNumbers nums2(0,100,50);
+	cout<< "=====Before mergeSort Sort=====" << endl;
+	nums2.displayGroupOfNumbers();
+	nums2.mergesort();	
+	cout<< "=====After mergeSort Sort=====" << endl;
+	nums2.displayGroupOfNumbers();
+	
+	//heapsort Test
+	GroupOfNumbers nums3(0,100,50);
+	cout<< "=====Before heapsort Sort=====" << endl;
+	nums3.displayGroupOfNumbers();
+	nums3.heapsort();	
+	cout<< "=====After heapsort Sort=====" << endl;
+	nums3.displayGroupOfNumbers();
+	
+	//quicksort Test
+	GroupOfNumbers nums4(0,100,50);
+	cout<< "=====Before quicksort Sort=====" << endl;
+	nums4.displayGroupOfNumbers();
+	nums4.heapsort();	
+	cout<< "=====After quicksort Sort=====" << endl;
+	nums4.displayGroupOfNumbers();
 }
