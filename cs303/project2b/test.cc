f11aaefffe99a8d9a9a5f1b91b4c8b3e35343344
eshih <ewshih@email.wm.edu>
2012-11-10 20:27:19 -0500

    3 quicksorts work

--- cs303/project2b/test.cc
+++ cs303/project2b/test.cc
@@ -2,15 +2,46 @@
 #include "group.h"
 
 using namespace std;
-
+extern double user_time();
+extern double system_time();
 
 int main (){
+	double start, finish;
+
 	//quicksort Test
 	GroupOfNumbers nums(0,100,20);
-	cout<< "=====Before quicksort Sort=====" << endl;
+	cout<< "*=====Before quicksort=====";
 	nums.displayGroupOfNumbers();
-	nums.quick_choose_first();	
-	//nums.quicksort();
-	cout<< "=====After quicksort Sort=====" << endl;
+	start = user_time() + system_time();
+	nums.quicksort();
+	finish = user_time() + system_time();
+	cout<< "=====After quicksort=====";
 	nums.displayGroupOfNumbers();
+	cout << "Start: " << start << " Finish: " << finish;
+	cout << "\nTiming: " << finish - start << endl;
+	
+	//quicksort first Test
+	GroupOfNumbers nums2(0,100,20);
+	cout<< endl << "*=====Before quicksort first=====";
+	nums2.displayGroupOfNumbers();
+	nums2.quick_choose_first();
+	cout<< "=====After quicksort first=====";
+	nums2.displayGroupOfNumbers();
+	
+	//quicksort middle Test
+	GroupOfNumbers nums3(0,100,20);
+	cout<< endl << "*=====Before quicksort middle=====";
+	nums3.displayGroupOfNumbers();
+	nums3.quick_choose_middle();	
+	cout<< "=====After quicksort middle=====";
+	nums3.displayGroupOfNumbers();
+	
+	//quicksort last Test
+	GroupOfNumbers nums4(0,100,20);
+	cout<< endl << "*=====Before quicksort last=====";
+	nums4.displayGroupOfNumbers();
+	nums4.quick_choose_last();	
+	cout<< "=====After quicksort last=====";
+	nums4.displayGroupOfNumbers();
+	
 }
